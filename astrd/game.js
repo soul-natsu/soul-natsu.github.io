2fe1c2d149307659bc729758e0208d68673b5873
soul-natsu <what.time.is.it.3f@gmail.com>
2018-05-17 08:06:01 -0400

    Update game.js

--- astrd/game.js
+++ astrd/game.js
@@ -288,11 +288,6 @@ Sprite = function () {
       px = trans[i*2];
       py = trans[i*2 + 1];
       // mozilla doesn't take into account transforms with isPointInPath >:-P
-      if (($.browser.mozilla) ? this.pointInPolygon(px, py) : this.context.isPointInPath(px, py)) {
-        other.collision(this);
-        this.collision(other);
-        return;
-      }
     }
   };
   this.pointInPolygon = function (x, y) {
